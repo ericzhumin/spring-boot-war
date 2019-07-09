24d7634af70816bf8e8555b238ac6bf5e4310a67
Victor.su <Victor.su@landasoft.com>
2019-07-09 17:07:31 +0800

    1,调整输出参数 2,增加短信接口测试

--- src/main/java/com/pivotal/example/demo/controller/RestBaseController.java
+++ src/main/java/com/pivotal/example/demo/controller/RestBaseController.java
@@ -1,16 +1,42 @@
 package com.pivotal.example.demo.controller;
 
+import com.pivotal.example.demo.entity.JgMessageEntity;
+import com.pivotal.example.demo.service.ShortMessageTool;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 /**
  * 欢迎控制器
+ *
  * @author wulinyun
  * @version 2017年9月8日下午3:28:30
  */
 @RestController
 public class RestBaseController {
-	@RequestMapping("/")
-	public String home(){
-		return "Hello World";
-	}
+
+//	@Autowired
+//	private ShortMessageTool shortMessageTool;
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello World";
+    }
+
+    @RequestMapping("/send")
+    public String send() {
+        String res=null;
+        try {
+            JgMessageEntity jgMessageEntity = new JgMessageEntity();
+            jgMessageEntity.setSubject("测试");
+            String iphonenume = "15900959575";
+            ShortMessageTool.sendMessage(iphonenume, jgMessageEntity);
+            res ="发送内容："+jgMessageEntity.getSubject()+",接收人手机："+iphonenume;
+
+        } catch (Exception ex) {
+
+        }
+
+        return res;
+    }
 }
