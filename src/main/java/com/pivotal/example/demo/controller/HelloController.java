29bb08e6577a22f5940867fdc534000b7163a04a
Victor.su <Victor.su@landasoft.com>
2019-07-10 10:31:28 +0800

    调整短信返回

--- src/main/java/com/pivotal/example/demo/controller/HelloController.java
+++ src/main/java/com/pivotal/example/demo/controller/HelloController.java
@@ -3,13 +3,20 @@ package com.pivotal.example.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+
 @Controller
 public class HelloController {
-	@RequestMapping("/hello")
-    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+    @RequestMapping("/hello")
+    public String greeting(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
         model.addAttribute("name", name);
         return "welcome";
     }
-   
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index() {
+        return "/index.html";
+    }
+
 }
